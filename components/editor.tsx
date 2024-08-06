4ddf9733cd4181c32c24ae4edd1657dd4ba34960
unknown <saadsyed950@gmail.com>
2024-08-06 19:43:44 +0530

    Editor changed

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useCreateBlockNote } from "@blocknote/react";
-import { BlockNoteView } from "@blocknote/mantine";
-import "@blocknote/mantine/style.css";
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import "@blocknote/core/style.css";
 import { useTheme } from "next-themes";
 
 import { useEdgeStore } from "@/lib/edgestore";
@@ -19,11 +19,18 @@ function Editor({ onChange, initialContent, editable }: EditorProps) {
 
   const handleUpload = async (file: File) => {
     const response = await edgestore.publicFiles.upload({ file });
+
     return response.url;
   };
 
-  const editor = useCreateBlockNote({
-    initialContent: initialContent ? JSON.parse(initialContent) : undefined,
+  const editor: BlockNoteEditor = useBlockNote({
+    editable,
+    initialContent: initialContent
+      ? (JSON.parse(initialContent) as PartialBlock[])
+      : undefined,
+    onEditorContentChange: (editor) => {
+      onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
+    },
     uploadFile: handleUpload,
   });
 
@@ -32,10 +39,6 @@ function Editor({ onChange, initialContent, editable }: EditorProps) {
       <BlockNoteView
         editor={editor}
         theme={resolvedTheme === "dark" ? "dark" : "light"}
-        editable={editable}
-        onChange={() => {
-          onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
-        }}
       />
     </div>
   );
